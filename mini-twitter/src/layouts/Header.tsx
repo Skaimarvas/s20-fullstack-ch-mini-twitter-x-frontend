ed0364553eb7e9ca218773c55a4449eef0342ed4
skaimarvas <skaimarvas@gmail.com>
2024-03-08 22:24:14 +0300

    tailwind css was added

--- mini-twitter/src/layouts/Header.tsx
+++ mini-twitter/src/layouts/Header.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center border-b border-gray-400 w-full px-5 py-2">
+    <div className="flex justify-between items-center border-b border-gray-300 w-full px-5 py-2">
       <h2 className="text-[20px] font-bold">Home</h2>
       <Icon icon="solar:star-fall-linear" className="text-[30px] " />
     </div>
