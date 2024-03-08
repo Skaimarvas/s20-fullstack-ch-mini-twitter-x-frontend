1037179e1fcfbce02ba21a1946a8365a6d759f71
skaimarvas <skaimarvas@gmail.com>
2024-03-08 13:24:26 +0300

    Footer was completed.

--- mini-twitter/src/layouts/Footer.tsx
+++ mini-twitter/src/layouts/Footer.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+//Icon
+import { Icon } from "@iconify/react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between gap-2 text-xs text-gray-500">
+        <span>Terms of Service</span>
+        <span>Privacy Policy</span>
+        <span>Cookie Policy</span>
+      </div>
+      <div className="flex justify-between gap-2 text-xs text-gray-500">
+        <span>Imprint</span>
+        <span>Ads Info</span>
+        <div className="flex justify-between items-center gap-1">
+          <span>More</span>
+          <Icon icon="icon-park-outline:more" className="text-[15px]" />
+        </div>
+        <div className="flex justify-between items-center gap-1">
+          <Icon icon="ph:copyright-light" className="text-[15px]" />
+          <span> 2021 Twitter, Inc</span>
+        </div>
+      </div>
+    </div>
+  );
 }
