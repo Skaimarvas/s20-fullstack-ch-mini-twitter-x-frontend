395093c9eb35a66122bbac61173e0bbef48ed0b2
skaimarvas <skaimarvas@gmail.com>
2024-03-08 14:47:09 +0300

    Header was completed

--- mini-twitter/src/layouts/Header.tsx
+++ mini-twitter/src/layouts/Header.tsx
@@ -1,3 +1,11 @@
+//Icon
+import { Icon } from "@iconify/react";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center border-b border-gray-400 w-full px-5 py-2">
+      <h2 className="text-[20px] font-bold">Home</h2>
+      <Icon icon="solar:star-fall-linear" className="text-[30px] " />
+    </div>
+  );
 }
