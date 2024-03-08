91cbff42b32f75f7b1d7dfa143c165a2a74d070d
skaimarvas <skaimarvas@gmail.com>
2024-03-08 14:47:27 +0300

    Header was added on MainSection

--- mini-twitter/src/layouts/MainSection.tsx
+++ mini-twitter/src/layouts/MainSection.tsx
@@ -1,3 +1,9 @@
+import Header from "./Header";
+
 export default function MainSection() {
-  return <div>MainSection</div>;
+  return (
+    <div className=" flex flex-col items-center w-2/5">
+      <Header />
+    </div>
+  );
 }
