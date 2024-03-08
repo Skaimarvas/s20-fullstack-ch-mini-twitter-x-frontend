0abb1548cacdf0dba89cd9108cae50910b351f85
skaimarvas <skaimarvas@gmail.com>
2024-03-08 13:31:06 +0300

    pt-4 was added

--- mini-twitter/src/layouts/SidebarRight.tsx
+++ mini-twitter/src/layouts/SidebarRight.tsx
@@ -23,7 +23,7 @@ export default function SidebarRight() {
     console.log(data);
   };
   return (
-    <div className="flex flex-col gap-2 ">
+    <div className="flex flex-col gap-2 pt-4">
       <div className="sticky">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="relative">
