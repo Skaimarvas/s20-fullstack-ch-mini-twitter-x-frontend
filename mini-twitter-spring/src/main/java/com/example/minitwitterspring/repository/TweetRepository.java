59a31281c26a198ef07cef18f7a9b22762280b50
skaimarvas <skaimarvas@gmail.com>
2024-03-10 16:04:27 +0300

    repositories were updated.

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/TweetRepository.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/TweetRepository.java
@@ -3,5 +3,5 @@ package com.example.minitwitterspring.repository;
 import com.example.minitwitterspring.entity.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TweetRepository extends JpaRepository<Long, Tweet> {
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
