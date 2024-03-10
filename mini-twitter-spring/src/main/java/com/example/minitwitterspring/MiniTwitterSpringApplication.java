10c78af986a2fb7445f18f132eafd32df25dc87e
skaimarvas <skaimarvas@gmail.com>
2024-03-10 15:13:44 +0300

    mini-twitter-spring project was created

---
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/MiniTwitterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.minitwitterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTwitterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTwitterSpringApplication.class, args);
+	}
+
+}
---
+++ mini-twitter-spring/src/test/java/com/example/minitwitterspring/MiniTwitterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minitwitterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTwitterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
