6565bac76151d5063695c9e4666c4c5b4d4a26c3
skaimarvas <skaimarvas@gmail.com>
2024-03-10 15:52:35 +0300

    all entities were completed

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/entity/Tweet.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/entity/Tweet.java
@@ -1,8 +1,7 @@
 package com.example.minitwitterspring.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,10 +11,20 @@ import lombok.NoArgsConstructor;
 @Table(name = "tweet", schema = "kiwitter")
 public class Tweet {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "text")
     private String text;
+    @Column(name = "like")
     private int like;
+    @Column(name = "retweet")
     private int retweet;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
 
 
 }
--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/entity/User.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/entity/User.java
@@ -1,4 +1,37 @@
 package com.example.minitwitterspring.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user", schema = "kiwitter")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Map<Long,Tweet> tweets = new HashMap<>();
+
+    private Map<Long,Tweet> likedTweets;
+
+    private Map<Long,Tweet> retweetedTweets;
+
+
 }
