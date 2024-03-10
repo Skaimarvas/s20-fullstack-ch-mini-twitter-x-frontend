295dee82dd9c8a2df1a11b341faa13005b36f6b3
skaimarvas <skaimarvas@gmail.com>
2024-03-10 12:23:04 +0300

    form was modified

--- mini-twitter/src/components/TweetInput.tsx
+++ mini-twitter/src/components/TweetInput.tsx
@@ -21,49 +21,52 @@ export const TweetInput: React.FC = () => {
       <div className="flex justify-center items-center">
         <img src={profile} alt="profile" className="w-12 h-12 rounded-full" />
       </div>
-      <div className="flex flex-col gap-2   w-full">
-        <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <div className="flex flex-col gap-2">
+          <div>
             <input
               {...register("tweet")}
               type="text"
               className="flex h-[100px] w-full outline-none px-2 text-wrap"
               placeholder="What's happening"
             />
-          </form>
-        </div>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-5 ">
-            <Icon
-              icon="clarity:image-line"
-              className="text-[25px] text-[#1D9BF0]"
-            />
-
-            <Icon
-              icon="material-symbols:gif-box-outline"
-              className="text-[25px] text-[#1D9BF0]"
-            />
-
-            <Icon
-              icon="foundation:graph-horizontal"
-              className="text-[25px] text-[#1D9BF0]"
-            />
-
-            <Icon
-              icon="fa-regular:smile"
-              className="text-[25px] text-[#1D9BF0]"
-            />
-            <Icon
-              icon="fluent-mdl2:date-time-12"
-              className="text-[25px] text-[#1D9BF0]"
-            />
           </div>
-          <button className="bg-[#1DA1F2] rounded-full py-2 px-6 ">
-            {" "}
-            <span className="text-white font-bold">Tweet</span>{" "}
-          </button>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-5 ">
+              <Icon
+                icon="clarity:image-line"
+                className="text-[25px] text-[#1D9BF0]"
+              />
+
+              <Icon
+                icon="material-symbols:gif-box-outline"
+                className="text-[25px] text-[#1D9BF0]"
+              />
+
+              <Icon
+                icon="foundation:graph-horizontal"
+                className="text-[25px] text-[#1D9BF0]"
+              />
+
+              <Icon
+                icon="fa-regular:smile"
+                className="text-[25px] text-[#1D9BF0]"
+              />
+              <Icon
+                icon="fluent-mdl2:date-time-12"
+                className="text-[25px] text-[#1D9BF0]"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-[#1DA1F2] rounded-full py-2 px-6 "
+            >
+              {" "}
+              <span className="text-white font-bold">Tweet</span>{" "}
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
