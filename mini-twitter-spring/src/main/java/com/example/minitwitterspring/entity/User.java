3dd965e8bbe474e415b60c3371b2295a7b597f33
skaimarvas <skaimarvas@gmail.com>
2024-03-13 23:11:51 +0300

    addTweets and addLikedTweets methods were added

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/entity/User.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/entity/User.java
@@ -1,11 +1,14 @@
 package com.example.minitwitterspring.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @NoArgsConstructor
@@ -26,12 +29,49 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @JsonManagedReference
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private Map<Long,Tweet> tweets = new HashMap<>();
+    private List<Tweet> tweets ;
 
-    private Map<Long,Tweet> likedTweets;
+    public void addTweets(Tweet tweet){
+        if(tweets == null){
+            tweets = new ArrayList<>();
+        }
+        this.tweets.add(tweet);
+    }
+    public void removeTweets(Tweet tweet){
+        if(tweets != null ){
+            tweets.remove(tweet);
+        }
+    }
 
-    private Map<Long,Tweet> retweetedTweets;
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Tweet> likedTweets ;
+
+
+    public void addLikedTweets(Tweet tweet){
+        if(likedTweets == null){
+            likedTweets = new ArrayList<>();
+        }
+
+        if (!likedTweets.contains(tweet)){
+            likedTweets.add(tweet);
+        }
+    }
+
+    public void removedLikedTweets(Tweet tweet){
+        if(likedTweets != null){
+            likedTweets.remove(tweet);
+        }
+
+
+    }
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Tweet> retweetedTweets = new ArrayList<>();
+
 
 
 }
--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/TweetRepository.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/TweetRepository.java
@@ -4,4 +4,5 @@ import com.example.minitwitterspring.entity.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
 }
