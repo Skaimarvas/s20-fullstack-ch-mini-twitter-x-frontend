9792662569c0441b94c242921a78ebab787efe8d
skaimarvas <skaimarvas@gmail.com>
2024-03-13 23:11:09 +0300

    TweetController was updated and its methods were modified.

---
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/controller/TweetController.java
@@ -0,0 +1,107 @@
+package com.example.minitwitterspring.controller;
+
+
+import com.example.minitwitterspring.entity.Tweet;
+import com.example.minitwitterspring.entity.User;
+import com.example.minitwitterspring.service.TweetService;
+import com.example.minitwitterspring.service.UserService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+/**Notlar:
+ * i)Tweet'i güncellediğimde otomatik user'ın tweets'lerinden de güncelliyor. Bu tweet'in kullanıcısından güncellemeye gerek yokmuş.
+ */
+
+@RestController
+@RequestMapping("/v1/api/kiwitter")
+public class TweetController {
+
+    private final TweetService tweetService;
+    private final UserService userService;
+
+    @Autowired
+    public TweetController(TweetService tweetService, UserService userService) {
+        this.tweetService = tweetService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/tweets")
+    public List<Tweet> listAll(){
+        return tweetService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Tweet findTweet(@PathVariable long id){
+        return tweetService.findById(id);
+    }
+
+    @PostMapping("/{userId}/tweet")
+    @Transactional
+    public Tweet postTweet(@PathVariable long userId,@RequestBody Tweet tweet){
+        User user = userService.findById(userId);
+        if(user != null){
+            user.addTweets(tweet);
+            tweet.setUser(user);
+            return tweetService.save(tweet);
+        }
+        return null;
+    }
+
+    @PutMapping("/update/{userId}/{tweetId}")
+    @Transactional
+    public Tweet updatedTweet(@RequestBody Tweet tweet, @PathVariable long tweetId, @PathVariable long userId ){
+        Tweet foundTweet = tweetService.findById(tweetId);
+        User foundUser = userService.findById(userId);
+        if(foundTweet != null && foundTweet.getUser().getId() == foundUser.getId()){
+            foundTweet.setContent(tweet.getContent());
+            return tweetService.save(foundTweet);
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/delete/{userId}/{tweetId}")
+    @Transactional
+    public Tweet removedTweet(@PathVariable long tweetId, @PathVariable long userId){
+        Tweet foundTweet = tweetService.findById(tweetId);
+        User foundUser = userService.findById(userId);
+        if(foundTweet != null && foundTweet.getUser().getId() == foundUser.getId()){
+            return tweetService.remove(tweetId);
+        }
+        return null;
+    }
+
+    @PostMapping("/like/{userId}/{tweetId}")
+    @Transactional
+    public Tweet likedTweet(@PathVariable long userId, @PathVariable long tweetId){
+        Tweet foundTweet = tweetService.findById(tweetId);
+        User foundUser = userService.findById(userId);
+        if(foundTweet != null && foundUser != null ){
+            foundTweet.setLikeTweet(foundTweet.getLikeTweet() + 1);
+            foundUser.addLikedTweets(foundTweet);
+            return foundTweet;
+        }
+        return foundTweet;
+    }
+
+    @PostMapping("/unlike/{userId}/{tweetId}")
+    @Transactional
+    public Tweet unLikedTweet(@PathVariable long userId, @PathVariable long tweetId){
+        Tweet foundTweet = tweetService.findById(tweetId);
+        User foundUser = userService.findById(userId);
+        if(foundTweet != null && foundTweet.getLikeTweet() > 0 && foundUser != null ){
+            foundTweet.setLikeTweet(foundTweet.getLikeTweet() - 1);
+            foundUser.removeTweets(foundTweet);
+            return foundTweet;
+        }
+        return foundTweet;
+    }
+
+
+}
