4df3ce2175dd0065adf313ee78c9642f3b87f2d0
skaimarvas <skaimarvas@gmail.com>
2024-03-16 23:01:53 +0300

    endpoint was updated and toast was added

--- mini-twitter/src/store/thunks/UserThunk.tsx
+++ mini-twitter/src/store/thunks/UserThunk.tsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 import { postSignupDataToApi } from "../actions/UserActions";
+import { toast } from "react-toastify";
 
 export const postSignupData = (user: any) => {
   return (dispatch: Dispatch) => {
     axios
-      .post("/v1/api/profile/register", user)
+      .post("http://localhost:9000/v1/api/profile/register", user)
       .then((res: any) => {
         dispatch(postSignupDataToApi(res.data));
+        console.log(res.data);
+        toast.success("You have succesfully signed up!");
       })
       .catch((err: any) => {
         console.log(err.message);
