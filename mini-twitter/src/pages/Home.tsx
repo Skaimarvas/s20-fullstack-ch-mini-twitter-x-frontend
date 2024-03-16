de17f2353f8a8f86abe1c17b7e7a565877e95bbe
skaimarvas <skaimarvas@gmail.com>
2024-03-16 23:00:58 +0300

    Home page was created

---
+++ mini-twitter/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Sidebar } from "../layouts/Sidebar";
+import { MainSection } from "../layouts/MainSection";
+import { SidebarRight } from "../layouts/SidebarRight";
+
+export const Home: React.FC = () => {
+  return (
+    <div className="flex flex-row justify-center">
+      <Sidebar />
+      <MainSection />
+      <SidebarRight />
+    </div>
+  );
+};
