998c3b6a68901a2e8795d303cf514300af633c86
skaimarvas <skaimarvas@gmail.com>
2024-03-17 17:34:10 +0300

    findByEmail query was added

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/UserRepository.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.example.minitwitterspring.repository;
 
 import com.example.minitwitterspring.entity.User;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(String email);
 }
