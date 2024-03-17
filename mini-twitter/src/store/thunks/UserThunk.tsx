2c8fa9e9eb8300bf8f932152cc25cd0564ba8608
skaimarvas <skaimarvas@gmail.com>
2024-03-17 17:33:20 +0300

    postLoginData was added

--- mini-twitter/src/store/thunks/UserThunk.tsx
+++ mini-twitter/src/store/thunks/UserThunk.tsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { Dispatch } from "redux";
-import { postSignupDataToApi } from "../actions/UserActions";
+import {
+  postLoginDataToApi,
+  postSignupDataToApi,
+} from "../actions/UserActions";
 import { toast } from "react-toastify";
 
 export const postSignupData = (user: any) => {
@@ -14,6 +17,25 @@ export const postSignupData = (user: any) => {
       })
       .catch((err: any) => {
         console.log(err.message);
+      });
+  };
+};
+
+export const postLoginData = (user: any) => {
+  return (dispatch: Dispatch) => {
+    axios
+      .post("http://localhost:9000/v1/api/profile/login", user)
+      .then((res: any) => {
+        dispatch(postLoginDataToApi(res.data));
+        console.log(res.data);
+        toast.success("You have succesfully login!");
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 2000);
+      })
+      .catch((err: any) => {
+        console.log(err);
+        toast.error(err.response.data.message);
       });
   };
 };
