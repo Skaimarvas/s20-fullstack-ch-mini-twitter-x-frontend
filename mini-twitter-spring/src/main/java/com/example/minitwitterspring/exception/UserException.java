5cea7f00e6a0fe0e6483f003eef12520b584f21c
skaimarvas <skaimarvas@gmail.com>
2024-03-17 17:33:34 +0300

    UserException was created

---
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/exception/UserException.java
@@ -0,0 +1,13 @@
+package com.example.minitwitterspring.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class UserException extends RuntimeException{
+    private HttpStatus status;
+    public UserException(String message, HttpStatus status){
+        super(message);
+        this.status = status;
+    }
+}
