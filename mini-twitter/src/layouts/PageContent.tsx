a28313c0072e1469c78e2ed08b24cf2a4a1b7af0
skaimarvas <skaimarvas@gmail.com>
2024-03-17 17:34:44 +0300

    PageContent was updated

--- mini-twitter/src/layouts/PageContent.tsx
+++ mini-twitter/src/layouts/PageContent.tsx
@@ -1,7 +1,9 @@
 import { Switch, Route } from "react-router-dom";
+
 //Pages
 import { Home } from "../pages/Home";
 import SignUpPage from "../pages/SignUpPage";
+import LoginPage from "../pages/LoginPage";
 
 export const PageContent: React.FC = () => {
   return (
@@ -9,6 +11,9 @@ export const PageContent: React.FC = () => {
       <Switch>
         <Route path="/signup">
           <SignUpPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
         </Route>
         <Route path="/">
           <Home />
