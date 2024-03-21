5a6de6558ab92d46fd722c68625838f91f4fdbf2
skaimarvas <skaimarvas@gmail.com>
2024-03-21 22:46:59 +0300

    mocttweet was modified

--- mini-twitter/src/mock/MockTweet.tsx
+++ mini-twitter/src/mock/MockTweet.tsx
@@ -1,8 +1,8 @@
 export const mockTweets: string[] = [
-  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California…<br/> Why?",
-  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California…<br/> Why?",
-  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California…<br/> Why?",
-  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California…<br/> Why?",
-  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California…<br/> Why?",
-  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California…<br/> Why?",
+  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California… Why?",
+  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California… Why?",
+  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California… Why?",
+  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California… Why?",
+  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California… Why?",
+  " Illegals beat up American police officers in Times Square, got out of jail for free and, instead of being deported, were given free tickets to California… Why?",
 ];
