8278af0bddc78b156febe5f47f84e1cfd61b027c
skaimarvas <skaimarvas@gmail.com>
2024-03-21 19:56:27 +0300

    mock tweets were created and added on MainSection

--- mini-twitter/src/layouts/MainSection.tsx
+++ mini-twitter/src/layouts/MainSection.tsx
@@ -1,3 +1,4 @@
+import { mockTweets } from "../mock/MockTweet";
 //Components
 import { TweetInput } from "../components/TweetInput";
 import { Tweet } from "../components/Tweet";
@@ -5,9 +6,9 @@ import { Tweet } from "../components/Tweet";
 import Header from "./Header";
 //Hooks
 import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/hook";
 //Thunks
 import { listTweets } from "../store/thunks/TweetThunk";
-import { useAppDispatch, useAppSelector } from "../hooks/hook";
 
 export const MainSection: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -20,49 +21,18 @@ export const MainSection: React.FC = () => {
     <div className=" flex flex-col items-center w-2/5">
       <Header />
       <TweetInput />
-
       {tweets &&
         tweets.map((tweet: any) => (
           <Tweet key={tweet.id}>
             <p className="text-sm text-gray-500">{tweet.content}</p>
           </Tweet>
         ))}
-      <Tweet>
-        <p className="text-sm text-gray-500">
-          Illegals beat up American police officers in Times Square, got out of
-          jail for free and, instead of being deported, were given free tickets
-          to California …
-          <br />
-          Why?
-        </p>{" "}
-      </Tweet>
-      <Tweet>
-        <p className="text-sm text-gray-500">
-          Illegals beat up American police officers in Times Square, got out of
-          jail for free and, instead of being deported, were given free tickets
-          to California …
-          <br />
-          Why?
-        </p>{" "}
-      </Tweet>
-      <Tweet>
-        <p className="text-sm text-gray-500">
-          Illegals beat up American police officers in Times Square, got out of
-          jail for free and, instead of being deported, were given free tickets
-          to California …
-          <br />
-          Why?
-        </p>{" "}
-      </Tweet>
-      <Tweet>
-        <p className="text-sm text-gray-500">
-          Illegals beat up American police officers in Times Square, got out of
-          jail for free and, instead of being deported, were given free tickets
-          to California …
-          <br />
-          Why?
-        </p>{" "}
-      </Tweet>
+      {mockTweets &&
+        mockTweets.map((tweet: string, index) => (
+          <Tweet key={index}>
+            <p className="text-sm text-gray-500">{tweet}</p>
+          </Tweet>
+        ))}
     </div>
   );
 };
