0c271f4362fd2bb5540bc23536abda7336a86b3a
skaimarvas <skaimarvas@gmail.com>
2024-04-21 22:16:32 +0300

    add TweePageContent

--- mini-twitter/src/layouts/MainSection.tsx
+++ mini-twitter/src/layouts/MainSection.tsx
@@ -1,39 +1,17 @@
-import { mockTweets } from "../mock/MockTweet";
 //Components
 import TweetInput from "../components/TweetInput";
-import Tweet from "../components/Tweet";
 //Layouts
 import Header from "./Header";
 //Hooks
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../hooks/hook";
 //Thunks
-import { listTweets } from "../store/thunks/TweetThunk";
-import { RootState } from "../store/store";
+import TweetPageContent from "./TweetPageContent";
 
 const MainSection: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const { tweets } = useAppSelector((store: RootState) => store.tweet);
-
-  useEffect(() => {
-    dispatch(listTweets());
-  }, []);
   return (
-    <div className=" flex flex-col items-center w-2/5">
+    <div className="flex flex-col items-center w-2/5 lg:w-3/5 md:w-5/6">
       <Header />
       <TweetInput />
-      {tweets &&
-        tweets.map((tweet: any) => (
-          <Tweet key={tweet.id}>
-            <p className="text-sm text-gray-500">{tweet.content}</p>
-          </Tweet>
-        ))}
-      {mockTweets &&
-        mockTweets.map((tweet: string, index) => (
-          <Tweet key={index}>
-            <p className="text-sm text-gray-500">{tweet}</p>
-          </Tweet>
-        ))}
+      <TweetPageContent />
     </div>
   );
 };
