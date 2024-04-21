250229b57a1930361ce50a8457e2bbebe671220a
skaimarvas <skaimarvas@gmail.com>
2024-04-21 22:16:47 +0300

    remove div

--- mini-twitter/src/layouts/PageContent.tsx
+++ mini-twitter/src/layouts/PageContent.tsx
@@ -7,19 +7,17 @@ import LoginPage from "../pages/LoginPage";
 
 const PageContent: React.FC = () => {
   return (
-    <div>
-      <Switch>
-        <Route path="/signup">
-          <SignUpPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/signup">
+        <SignUpPage />
+      </Route>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 };
 
