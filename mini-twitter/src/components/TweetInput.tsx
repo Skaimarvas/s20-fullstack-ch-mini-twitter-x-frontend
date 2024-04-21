a632a5c6e2bcf6d639a47323ba7ba6dac392771b
skaimarvas <skaimarvas@gmail.com>
2024-04-21 22:14:55 +0300

    add buttons on icons

--- mini-twitter/src/components/TweetInput.tsx
+++ mini-twitter/src/components/TweetInput.tsx
@@ -22,20 +22,25 @@ const TweetInput: React.FC = () => {
   const userLocalStorage = localStorage.getItem("userData");
   const userLS = userLocalStorage ? JSON.parse(userLocalStorage) : null;
   const dispatch = useAppDispatch();
-  const { register, handleSubmit } = useForm<TweetFormData>();
+  const { register, handleSubmit, reset } = useForm<TweetFormData>();
   const onSubmit: SubmitHandler<TweetFormData> = (data) => {
     dispatch(postTweetData(userLS?.id, data));
+    reset();
   };
   console.log(userLS);
 
   return (
-    <div className="flex gap-2 justify-start items-start w-full px-3 py-2 border-b border-gray-300">
+    <div className="flex gap-2 justify-start items-start px-3 py-2 border-b border-gray-300 w-full">
       <div className="flex justify-center items-center">
-        <img src={profile} alt="profile" className="w-12 h-12 rounded-full" />
+        <img
+          src={profile}
+          alt="profile"
+          className="min-w-12 min-h-12 max-w-12 max-h-12 rounded-full"
+        />
       </div>
       <form onSubmit={handleSubmit(onSubmit)} className="w-full">
         <div className="flex flex-col gap-2">
-          <div>
+          <div className="border-b border-gray-300">
             <input
               {...register("content")}
               type="text"
@@ -43,35 +48,42 @@ const TweetInput: React.FC = () => {
               placeholder="What's happening"
             />
           </div>
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-5 ">
-              <Icon
-                icon="clarity:image-line"
-                className="text-[25px] text-[#1D9BF0]"
-              />
-
-              <Icon
-                icon="material-symbols:gif-box-outline"
-                className="text-[25px] text-[#1D9BF0]"
-              />
-
-              <Icon
-                icon="foundation:graph-horizontal"
-                className="text-[25px] text-[#1D9BF0]"
-              />
-
-              <Icon
-                icon="fa-regular:smile"
-                className="text-[25px] text-[#1D9BF0]"
-              />
-              <Icon
-                icon="fluent-mdl2:date-time-12"
-                className="text-[25px] text-[#1D9BF0]"
-              />
+          <div className="flex flex-wrap justify-between items-center">
+            <div className="flex  items-center  gap-1 ">
+              <button className="rounded-full hover:bg-gray-300 p-2  hover:transition">
+                <Icon
+                  icon="clarity:image-line"
+                  className="text-[20px] text-[#1D9BF0]"
+                />
+              </button>
+              <button className="rounded-full hover:bg-gray-300 p-2  hover:transition">
+                <Icon
+                  icon="material-symbols:gif-box-outline"
+                  className="text-[20px] text-[#1D9BF0]"
+                />
+              </button>
+              <button className="rounded-full hover:bg-gray-300 p-2  hover:transition">
+                <Icon
+                  icon="foundation:graph-horizontal"
+                  className="text-[20px] text-[#1D9BF0]"
+                />
+              </button>
+              <button className="rounded-full hover:bg-gray-300 p-2  hover:transition">
+                <Icon
+                  icon="fa-regular:smile"
+                  className="text-[20px] text-[#1D9BF0]"
+                />
+              </button>
+              <button className="rounded-full hover:bg-gray-300 p-2  hover:transition">
+                <Icon
+                  icon="fluent-mdl2:date-time-12"
+                  className="text-[20px] text-[#1D9BF0]"
+                />
+              </button>{" "}
             </div>
             <button
               type="submit"
-              className="bg-[#1DA1F2] rounded-full py-2 px-6 "
+              className="bg-[#1DA1F2] hover:bg-[#1d9cf0bb] rounded-full py-2 px-6 "
             >
               {" "}
               <span className="text-white font-bold">Tweet</span>{" "}
