b7f9637a76f6f2f182a600320c2bca21da068404
skaimarvas <skaimarvas@gmail.com>
2024-04-21 22:13:18 +0300

    add apply sidebarmenu css

--- mini-twitter/src/components/SideBarMenu.tsx
+++ mini-twitter/src/components/SideBarMenu.tsx
@@ -4,53 +4,57 @@ import { Icon } from "@iconify/react";
 
 const SideBarMenu: React.FC = () => {
   return (
-    <div className="flex flex-col gap-[20px] font-bold">
-      <div className="flex gap-4 items-center">
+    <div className="flex flex-col gap-[10px] font-bold lg:items-center ">
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="mingcute:home-5-line" className="text-[30px]" />
-        <span>Home</span>{" "}
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px]  ">Home</span>{" "}
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="iconoir:hashtag" className="text-[30px]" />
-        <span>Explore</span>{" "}
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px] ">Explore</span>{" "}
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="tdesign:notification" className="text-[30px]" />
-        <span>Notification</span>
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px] ">Notification</span>
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="lets-icons:message" className="text-[30px]" />
-        <span>Messages</span>
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px] ">Messages</span>
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon
           icon="material-symbols:bookmark-outline"
           className="text-[30px]"
         />
-        <span>Bookmarks</span>
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px] ">Bookmarks</span>
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="ri:file-list-2-line" className="text-[30px]" />
-        <span>Lists</span>
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px] ">Lists</span>
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="iconamoon:profile-fill" className="text-[30px]" />
-        <span>Profile</span>
-      </div>
-      <div className="flex gap-4 items-center">
+        <span className="lg:hidden text-[20px] ">Profile</span>
+      </button>
+      <button className="sidebarmenu">
         {" "}
         <Icon icon="mingcute:more-3-line" className="text-[30px]" />
-        <span>More</span>
-      </div>
-      <button className="text-white bg-[#1D9BF0] rounded-full px-[70px] py-2">
+        <span className="lg:hidden text-[20px] ">More</span>
+      </button>
+      <button className="text-white bg-[#1D9BF0] hover:bg-[#1d9cf0bb]   rounded-full px-[70px] lg:p-[10px] lg:flex lg:justify-center py-2 w-full lg:w-fit ">
         {" "}
-        Tweet
+        <span className="lg:hidden">Tweet</span>
+        <Icon
+          className="2xl:hidden lg:block lg:text-[20px]"
+          icon="fa-solid:feather-alt"
+        />
       </button>
     </div>
   );
