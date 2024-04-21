62665c53b60d52cd59d487874ff87cc67295f413
skaimarvas <skaimarvas@gmail.com>
2024-04-21 22:17:34 +0300

    add div

--- mini-twitter/src/layouts/SidebarRight.tsx
+++ mini-twitter/src/layouts/SidebarRight.tsx
@@ -11,11 +11,13 @@ import Suggestion from "../components/Suggestion";
  */
 const SidebarRight: React.FC = () => {
   return (
-    <div className="flex flex-col gap-2 pt-4 pl-8 border-l border-gray-300">
-      <SearchBar />
-      <Trends />
-      <Suggestion />
-      <Footer />
+    <div className="border-l border-gray-300  ">
+      <div className="flex flex-col gap-2 pt-4 pl-8  sticky right-0 top-0 lg:hidden">
+        <SearchBar />
+        <Trends />
+        <Suggestion />
+        <Footer />
+      </div>
     </div>
   );
 };
