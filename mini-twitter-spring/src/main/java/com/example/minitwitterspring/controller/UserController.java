18ed80982ea434765a75df489619f4bdbbbfe012
Kaim Arvas <skaimarvas@gmail.com>
2024-05-01 19:08:39 +0300

    add TweetUserDto type and UserDtoConverter

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/controller/UserController.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.minitwitterspring.controller;
 
+import com.example.minitwitterspring.dto.TweetUserDto;
 import com.example.minitwitterspring.dto.UserDto;
 import com.example.minitwitterspring.entity.User;
 import com.example.minitwitterspring.exception.UserException;
 import com.example.minitwitterspring.service.UserService;
+import com.example.minitwitterspring.utils.UserDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,29 +23,26 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public User register(@RequestBody User user){
-        return userService.save(user);
+    public TweetUserDto register(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        return UserDtoConverter.getInstance().toTweetUserDto(savedUser);
     }
 
     @PostMapping("/login")
-    public User login(@RequestBody UserDto userDto){
-
+    public TweetUserDto login(@RequestBody UserDto userDto) {
         User loginUser = userService.findByEmail(userDto.email());
-
-        if(loginUser != null && loginUser.getPassword().equals(userDto.password())){
-            return  loginUser;
+        if (loginUser != null && loginUser.getPassword().equals(userDto.password())) {
+            return UserDtoConverter.getInstance().toTweetUserDto(loginUser);
         }
-
         throw new UserException("Email or Password is wrong", HttpStatus.BAD_REQUEST);
-
     }
 
     @PostMapping("/logout")
-    public User logout(@RequestBody User user){
+    public User logout(@RequestBody User user) {
         User loginUser = userService.findById(user.getId());
 
-        if(loginUser != null){
-            return  loginUser;
+        if (loginUser != null) {
+            return loginUser;
         }
 
         return null;
