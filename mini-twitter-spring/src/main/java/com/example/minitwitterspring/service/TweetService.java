de4c3156afcdf7afeb925102d7b68dc45d42caa8
Kaim Arvas <skaimarvas@gmail.com>
2024-05-01 19:11:38 +0300

    add TweetDto type

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/service/TweetService.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/service/TweetService.java
@@ -1,14 +1,15 @@
 package com.example.minitwitterspring.service;
 
+import com.example.minitwitterspring.dto.TweetDto;
 import com.example.minitwitterspring.entity.Tweet;
 
 import java.util.List;
 
 public interface TweetService {
-    List<Tweet> findAll();
-    Tweet save(Tweet tweet);
+    List<TweetDto> findAll();
+    TweetDto save(Tweet tweet);
 
     Tweet findById(long id);
 
-    Tweet remove(long id);
+    TweetDto remove(long id);
 }
