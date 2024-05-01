721ce56a1a7ee37a292e9734f0d1c1c33b6003d8
Kaim Arvas <skaimarvas@gmail.com>
2024-05-01 19:10:52 +0300

    prettier

--- mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/TweetRepository.java
+++ mini-twitter-spring/src/main/java/com/example/minitwitterspring/repository/TweetRepository.java
@@ -3,6 +3,9 @@ package com.example.minitwitterspring.repository;
 import com.example.minitwitterspring.entity.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
 
 }
