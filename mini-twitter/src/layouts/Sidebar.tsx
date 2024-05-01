f29bd7593c46da7083b7837b0f529795e140fb8e
Kaim Arvas <skaimarvas@gmail.com>
2024-05-01 19:05:33 +0300

    add Link router

--- mini-twitter/src/layouts/Sidebar.tsx
+++ mini-twitter/src/layouts/Sidebar.tsx
@@ -3,15 +3,19 @@ import { Icon } from "@iconify/react";
 //Component
 import SideBarMenu from "../components/SideBarMenu";
 import Profile from "../components/Profile";
+import { Link } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
   return (
     <div className="flex flex-col gap-5 pt-5 p-8 border-r border-gray-300 h-screen sticky left-0 top-0  lg:px-2 lg:items-center">
       <div className="px-2">
-        <button className="flex lg:justify-center hover:bg-gray-300 p-[10px] rounded-full hover:transition w-fit">
+        <Link
+          to="/"
+          className="flex lg:justify-center hover:bg-gray-300 p-[10px] rounded-full hover:transition w-fit"
+        >
           {" "}
           <Icon icon="logos:twitter" className="text-[30px]" />{" "}
-        </button>
+        </Link>
       </div>
       <SideBarMenu />
       <Profile />
